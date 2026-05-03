808d654fd6db6f267055503eb7e67cb5ebce8f9d
Ch-ar1 <muhammada.rehman127@gmail.com>
2026-05-03 12:49:58 +0500

    Made improvements in main_gui.cpp

--- main_gui.cpp
+++ main_gui.cpp
@@ -3,25 +3,26 @@
 #include "DBManager.h"
 #include "AuthService.h"
 
+//Main starting
 int main(int argc, char* argv[]) {
     QApplication app(argc, argv);
     app.setApplicationName("AuctionHub");
     app.setApplicationVersion("1.0");
 
-    // Load persisted data
+    // Loading persisted data
     DBManager& db = DBManager::getInstance();
     db.loadUsers();
     db.loadItems();
     db.loadBids();
     db.loadNotifications();
 
-    // Create default admin if no users exist
+    // Create default admin and users if no users exist
     int count = 0;
     db.getAllUsers(count);
     if (count == 0) {
         AuthService::signup("admin", "admin123", "admin");
     }
-
+//show window
     MainWindow window;
     window.show();
 
